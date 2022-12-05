a2568b865db71441aaac68365666fd882ccf054f
Elizabeth Atkins <elizabeth.atkins@sinclair.edu>
2022-12-04 20:13:27 -0500

    Creating char array

--- Exceptions/Source.cpp
+++ Exceptions/Source.cpp
@@ -3,10 +3,12 @@
 
 #include <iostream>
 using namespace std;
+const int SIZE = 52;
 
 char character(char, int);
 int main() {
 
+	char alphabet[SIZE] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 	char s;
 	int o;
 
